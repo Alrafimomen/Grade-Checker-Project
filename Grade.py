ec62f0cbcd507ab3b60b211f03eb3bfe5d203281
Md Al Rafi Momen <280502819+Alrafimomen@users.noreply.github.com>
2026-04-30 04:26:10 +0600

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,12 @@
+print ("Welcome To Grade Checker Project")
+marks = float(input("Enter Your Marks:"))
+if(marks>=80) :
+    print("Excellent,You Got A+")
+elif (marks>=70) :
+    print("Very Good,You GOt A")
+elif (marks>=60) :
+    print("Good,You Got B")
+elif (marks>=50) :
+    print("You Got C")
+else :
+    print("Fail")
